c7bdb6d1d8dded8d72709c48b6613f01b57eb528
viniciusdds <viniciusdamasceno180@hotmail.com>
2021-09-24 16:58:44 -0300

    aula 450

--- lib/screens/campo_minado_app.dart
+++ lib/screens/campo_minado_app.dart
@@ -1,15 +1,56 @@
+import 'package:campo_minado/components/campo_widget.dart';
+import 'package:campo_minado/components/resultado_widget.dart';
+import 'package:campo_minado/models/campo.dart';
+import 'package:campo_minado/models/explosao_exception.dart';
 import 'package:flutter/material.dart';
 
+
 class CampoMinadoApp extends StatelessWidget {
+
+
+
+  void _reiniciar(){
+    print('reiniciar...');
+  }
+
+  void _abrir(Campo campo){
+    print('abrir...');
+  }
+
+  void _alternarMarcacao(Campo campo){
+    print('alternar marcação...');
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    Campo vizinho1 = Campo(linha: 1, coluna: 0);
+    vizinho1.minar();
+
+    Campo vizinho2 = Campo(linha: 1, coluna: 0);
+    vizinho2.minar();
+
+    Campo campo = Campo(linha: 0, coluna: 0);
+    campo.adicionarVizinho(vizinho1);
+    campo.adicionarVizinho(vizinho2);
+
+    try{
+      //campo.minar();
+      campo.alternarMarcacao();
+    } on ExplosaoException {}
+    
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Campo Minado'),
+        appBar: ResultadoWidget(
+          venceu: false,
+          onReiniciar: _reiniciar,
         ),
         body: Container(
-          child: Text('Tabuleiro'),
+          child: CampoWidget(
+            campo: campo,
+            onAbrir: _abrir,
+            onAlternarMarcacao: _alternarMarcacao,
+          ),
         ),
       ),
     );
